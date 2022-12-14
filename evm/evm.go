5486a791b2e0b6cfb6379f3780e6904e6cfa8d5e
olawolu <gbemigaolawolu@gmail.com>
2022-12-14 11:08:15 +0100

    basic vm setup

--- evm/evm.go
+++ evm/evm.go
@@ -2,26 +2,42 @@ package evm
 
 import "github.com/holiman/uint256"
 
-type EVM struct {
-	context     *ExecutionContext
-	interpreter *Interpreter
+type VM struct {
+	context      *executionContext
+	instructions InstructionSet
 }
 
-func NewEvm(ctx *ExecutionContext) *EVM {
-	evm := &EVM{
-		context: ctx,
+func NewVm(ctx *executionContext) *VM {
+	evm := &VM{
+		context:      ctx,
+		instructions: loadInstructionSet(),
 	}
-	evm.interpreter = NewInterpreter(evm)
 	return evm
 }
 
-func (*EVM) execute(code []byte) (stack []uint256.Int, returnData string, success bool) {
-	return
+func (vm *VM) Execute(code []byte) ([]uint256.Int, string, bool) {
+	var stack []uint256.Int
+
+	for !vm.context.stopped && vm.context.pc < uint64(len(code)) {
+		// Read the next opcode
+		opCode := decodeOp(vm.context)
+		op := vm.instructions[opCode]
+
+		stack = op.execute(vm.context)
+		vm.context.pc++
+	}
+
+	return stack, "", vm.context.stopped
 }
 
 func Run(code []byte) (stack []uint256.Int, returnData string, success bool) {
-	ctx := &ExecutionContext{}
-	vm := NewEvm(ctx)
-	stack, returnData, success = vm.execute(code)
+	ctx := NewExecutionContext(code)
+	vm := NewVm(ctx)
+	stack, returnData, success = vm.Execute(code)
 	return
+}
+
+func decodeOp(ctx *executionContext) OpCode {
+	op := ctx.ReadCode(1)
+	return OpCode(op[0])
 }
